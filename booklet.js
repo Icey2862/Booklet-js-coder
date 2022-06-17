3eb6f36ba619b1b934f826d570c202b6abc715c2
Brian Ojeda <80053280+Icey2862@users.noreply.github.com>
2022-06-17 17:37:42 -0300

    Resolviendo-Ejercicios

--- booklet.js
+++ booklet.js
@@ -1,4 +1,5 @@
-//Actividad 1 booklet
+/*Actividad 1 booklet 
+Crear tres (3) variables para asignar el nombre, apellido y edad de Homero, respectivamente. Es indistinto el empleo de var o  let en la declaración./*
 
 /*let nombre = "Homero "
 let apellido = "Simpson "
@@ -6,7 +7,9 @@ let edad = "48"
 let homero = nombre + apellido + edad
 console.log(homero)*/
 
-//Actividad 2 booklet
+/*Actividad 2 booklet
+Crear cinco (5) variables constantes, asignando a cada una de ellas diferentes nombres de ciudades. Generalmente, dichas variables se declaran con const.*/
+
 
 /*const cityOne = "Londres "
 const cityTwo = "California "
@@ -16,12 +19,34 @@ const cityFive = "Indiana"
 let citys = cityOne + cityTwo + cityThree + cityFour + cityFive
 console.log(citys)*/
 
-//Actividad 3 booklet
+/*Actividad 3 booklet
+Declarar variables para representar la información de un carnet de conducir.
+Luego, concatenarlas y asignar el resultado de la operación a una variable denominada carnet.*/
 
 /*let nombre = "Carlos ", apellido = "Meza ", edad = "22 ", fechaDeNacimiento = "24/11/05 ", pais = "Argentina "
 const domicilio = "Calle falsa 123"
 let carnet = "Nombre:" +nombre+ "Apellido:"+ apellido+ "Edad:" +edad+ "Fecha de Nacimiento:"+fechaDeNacimiento+ "Pais:" +pais + "Domicilio:"+domicilio
-
 console.log(carnet)*/
 
+/*Actividad 4 booklet
+Solicitar al usuario cinco (5) nombres pertenecientes a integrantes de una familia. Luego, concatenar dichas entradas y efectuar una salida por alerta.*/
 
+/*let integrante1 = prompt("Ingrese el nombre del primer integrante")
+let integrante2 = prompt("Ingrese el nombre del segundo integrante")
+let integrante3 = prompt("Ingrese el nombre del tercer integrante")
+let integrante4 = prompt("Ingrese el nombre del cuarto integrante")
+let integrante5=prompt("Ingrese el nombre del quinto integrante")
+
+let familia = integrante1 + " "+ integrante2 + " "+integrante3 + " "+integrante4 + " "+integrante5
+alert("Los miembros de la familia son:" + familia)*/
+
+/*Actividad 5 booklet
+Solicitar al usuario uno o más precios.
+Luego, efectuar un descuento en porcentajes (20%, 30%, etcétera) sobre los valores ingresados, y realizar una salida.*/
+
+/*let precio = parseFloat(prompt("Ingrese el valor del producto"))
+let descuento1 = precio - (precio * 0.2)
+
+alert("El valor es: " + descuento1)*/
+
+//---------------------------------------------------------------------------------------------------------------------
