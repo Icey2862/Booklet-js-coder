b82e700824c0c860b27a38da9d453d42cadb51b9
Brian Ojeda <80053280+Icey2862@users.noreply.github.com>
2022-06-17 17:20:43 -0300

    Comenzando booklet

---
+++ booklet.js
@@ -0,0 +1,27 @@
+//Actividad 1 booklet
+
+/*let nombre = "Homero "
+let apellido = "Simpson "
+let edad = "48"
+let homero = nombre + apellido + edad
+console.log(homero)*/
+
+//Actividad 2 booklet
+
+/*const cityOne = "Londres "
+const cityTwo = "California "
+const cityThree = "Los Angeles "
+const cityFour = "Nuevo Mexico "
+const cityFive = "Indiana"
+let citys = cityOne + cityTwo + cityThree + cityFour + cityFive
+console.log(citys)*/
+
+//Actividad 3 booklet
+
+/*let nombre = "Carlos ", apellido = "Meza ", edad = "22 ", fechaDeNacimiento = "24/11/05 ", pais = "Argentina "
+const domicilio = "Calle falsa 123"
+let carnet = "Nombre:" +nombre+ "Apellido:"+ apellido+ "Edad:" +edad+ "Fecha de Nacimiento:"+fechaDeNacimiento+ "Pais:" +pais + "Domicilio:"+domicilio
+
+console.log(carnet)*/
+
+
